cfc6909d1a2781c9f3c4f3c2316095570c700c2e
Shamim Ahammad <shamimfstack@gmail.com>
2024-01-05 20:26:23 +0600

    banner section added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 import "./Banner.css";
 // import BannerTemplate from "./BannerTemplate/BannerTemplate";
 // import plumberImg from "../../../../public/images/plumbing.jpg";
@@ -7,61 +7,29 @@ import "./Banner.css";
 // import paintingImg from "../../../../public/images/painting.jpg";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Banner = () => {
   const style = {
-    backgroundImage: "https://i.ibb.co/MCmq9PS/banner-img.jpg"
+    // backgroundImage: "url(https://i.ibb.co/MCmq9PS/banner-img.jpg)",
   };
   return (
-    // <Swiper
-    //   spaceBetween={50}
-    //   slidesPerView={3}
-    //   onSlideChange={() => console.log('slide change')}
-    //   onSwiper={(swiper) => console.log(swiper)}
-    // >
-    //   <SwiperSlide>
-    //     <BannerTemplate image={plumberImg} title="Plumber"></BannerTemplate>
-    //   </SwiperSlide>
-    //   <SwiperSlide>
-    //     <BannerTemplate image={electricalImg} title={"Electrical"}></BannerTemplate>
-    //   </SwiperSlide>
-    //   <SwiperSlide>
-    //     <BannerTemplate image={carpentryImg} title={"Carpenter"}></BannerTemplate>
-    //   </SwiperSlide>
-    //   <SwiperSlide>
-    //     <BannerTemplate image={paintingImg} title={"Painting"}></BannerTemplate>
-    //   </SwiperSlide>
-
-    // </Swiper>
-    <div className="h-screen flex justify-center items-center" style={style}>
-      <Swiper
-        spaceBetween={30}
-        effect={'fade'}
-        navigation={true}
-        pagination={{
-          clickable: true,
-        }}
-        // modules={[EffectFade, Navigation, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-      </Swiper>
+    <div className="md:h-screen flex flex-col-reverse md:flex-row justify-between py-8 items-center bg-gradient-to-r from-[#07607c] to-[#41caf4]" style={style}>
+      <div className="space-y-2 mx-auto">
+        <h2 className="text-3xl md:text-4xl text-white font-bold text-center">Make Your Career as Your Passion</h2>
+        <p className="text-xl font-semibold text-white text-center">Here are a lot of opportunities to choice as your career </p>
+        <div className="flex justify-center">
+        <button className="bg-green-400 text-white font-bold px-2 md:px-4 lg:px-5 py-2 md:py-3 rounded-xl">
+          Let's Explore
+        </button>
+        </div>
+      </div>
+      <div className="mx-auto">
+        <img className="w-2/3 mx-auto" src="https://i.ibb.co/L1Ykv2S/storyset-removebg-preview.png" alt="" />
+      </div>
     </div>
   );
 };
